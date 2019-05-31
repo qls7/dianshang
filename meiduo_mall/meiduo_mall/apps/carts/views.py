674d5de3b52a2a4d31750de1e7bd9f8dd31468d6
qsl7 <qls7@protonmail.com>
2019-05-31 16:37:34 +0800

    update

--- meiduo_mall/meiduo_mall/apps/carts/views.py
+++ meiduo_mall/meiduo_mall/apps/carts/views.py
@@ -14,8 +14,86 @@ from goods.models import SKU
 from meiduo_mall.utils.response_code import RETCODE
 
 
+class CartsSelectAllView(View):
+    """全选购物车"""
+
+    def put(self, request):
+        """
+        全选购物车
+        :param request:
+        :return:
+        """
+        # 1.获取参数
+        json_dict = json.loads(request.body.decode())
+        selected = json_dict.get('selected')
+        # 2.校验参数
+        if not selected:
+            
+        # 3.判断用户是否登录
+        # 4.如果登录成功
+        # 5.连接redis 获取hash的数据
+        # 6.判断selected的值
+        # 7.如果是True把所有的key值都加入到set中
+        # 8.如果都是False把所有的key都删除
+        # 9.返回
+        # 10.如果登录不成功
+        # 11.获取cookie的值,解密
+        # 12.把selected的值重新赋值给字典中的selected
+        # 13.加密
+        # 14.返回
+
 class CartsView(View):
     """购物车管理"""
+
+    def delete(self, request):
+        """
+        删除购物车里面的数据
+        :param request:
+        :return:
+        """
+        # 获取参数
+        json_dict = json.loads(request.body.decode())
+        sku_id = json_dict.get('sku_id')
+        # 校验参数
+        try:
+            SKU.objects.get(id=sku_id)
+        except SKU.DoesNotExist:
+            return http.HttpResponseForbidden('sku_id 不存在')
+        # 判断用户是否登录
+        if request.user.is_authenticated:
+            # 如果登录
+            # 链接redis 对hash和set里的数据进行删除
+            redis_conn = get_redis_connection('carts')
+            pl = redis_conn.pipeline()
+            pl.hdel('carts_%s' % request.user.id, sku_id)
+            pl.srem('selected_%s' % request.user.id, sku_id)
+
+            pl.execute()
+            # 返回
+            return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'ok'})
+
+        else:
+            # 如果没有登录
+            cookie_cart = request.COOKIES.get('carts')
+
+            if cookie_cart:
+                # 获取cookie中的值
+                # 解密
+                cart_dict = pickle.loads(base64.b64decode(cookie_cart.encode()))
+            else:
+                cart_dict = {}
+
+            response = http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'ok'})
+
+            # 删除对应的键的值
+            if sku_id in cart_dict:
+                del cart_dict[sku_id]
+                # 重新加密cookie
+                carts = base64.b64encode(pickle.dumps(cart_dict)).decode()
+                # 设置cookie
+                response.set_cookie('carts', carts)
+            # 返回
+            return response
 
     def put(self, request):
         """修改购物车"""
@@ -23,7 +101,7 @@ class CartsView(View):
         json_dict = json.loads(request.body.decode())
         sku_id = json_dict.get('sku_id')
         count = json_dict.get('count')
-        selected = json_dict.get('selected', default=True)
+        selected = json_dict.get('selected', True)
         # 校验参数
         if not all([sku_id, count]):
             return http.HttpResponseForbidden('缺少必传参数')
@@ -35,9 +113,9 @@ class CartsView(View):
             count = int(count)
         except Exception:
             return http.HttpResponseForbidden('count参数类型错误')
-        # if selected:
-        #     if not isinstance(selected, bool):
-        #         return http.HttpResponseForbidden('selected参数错误')
+        if selected:
+            if not isinstance(selected, bool):
+                return http.HttpResponseForbidden('selected参数错误')
         # 判断用户是否登录
         if request.user.is_authenticated:
             # 如果登录了
@@ -57,13 +135,13 @@ class CartsView(View):
                 'id': sku.id,
                 'name': sku.name,
                 'count': count,
-                'selected': str(selected),
+                'selected': selected,
                 'default_image_url': sku.default_image_url,
-                'price': str(sku.price),
-                'amount': str(sku.price * count),
+                'price': sku.price,
+                'amount': sku.price * count,
 
             }
-            return http.JsonResponse({'code':RETCODE.OK,'errmsg':'ok','cart_sku':cart_sku})
+            return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'ok', 'cart_sku': cart_sku})
         else:
             # 如果没有登录
             # 从cookie中取值
@@ -92,15 +170,11 @@ class CartsView(View):
                 'amount': sku.price * count,
 
             }
-            context = {
-                'code': RETCODE.OK,
-                'errmsg': 'OK',
-                'cart_sku': cart_sku
-            }
             # 设置cookie
-            response = render(request, 'cart.html', context=context)
+            response = http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'ok', 'cart_sku': cart_sku})
+
             carts = base64.b64encode(pickle.dumps(carts_dict)).decode()
-            response.set_cookie('carts', carts,max_age=3600*24*15)
+            response.set_cookie('carts', carts)
             # 返回
             return response
 
@@ -139,10 +213,10 @@ class CartsView(View):
                 carts_dict = {}
 
         # 对字典进行遍历取出key和value进行拼接数据返回
-        carts_skus = list()
+        cart_skus = list()
         skus = SKU.objects.filter(id__in=carts_dict.keys())
         for sku in skus:
-            carts_skus.append({
+            cart_skus.append({
                 'id': sku.id,
                 'name': sku.name,
                 'count': carts_dict.get(sku.id).get('count'),
@@ -154,7 +228,7 @@ class CartsView(View):
 
         context = {
 
-            'cart_skus': carts_skus
+            'cart_skus': cart_skus
         }
         return render(request, 'cart.html', context=context)
 
@@ -198,17 +272,7 @@ class CartsView(View):
                 pl.sadd('selected_%s' % request.user.id, sku_id)
             pl.execute()
 
-            return http.JsonResponse({'code':RETCODE.OK,'errmsg':'ok'})
-            # 拼接格式,跟cookie一样的格式
-            # sku_id1:{
-            #   'count':count
-            #   'selected':bool
-            # }
-            # cart_dict = dict()
-            # cart_dict[int(sku_id)] = {
-            #     'count': count,
-            #     'selected': str(selected)
-            # }
+            return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'ok'})
 
         else:
             # 如果没登录
@@ -217,24 +281,24 @@ class CartsView(View):
             # 判断是否有cookie
             if carts:
                 # 如果有进行解码生成字典
-                cart_dict = pickle.loads(base64.b64decode(carts))
+                cart_dict = pickle.loads(base64.b64decode(carts.encode()))
 
             else:
                 # 如果没有新建一个字典
                 cart_dict = dict()
             # 对字典存在这个id值,直接进行添加
             if sku_id in cart_dict:
-                count += cart_dict[int(sku_id)]['count']
+                count += cart_dict[sku_id]['count']
 
-            cart_dict[int(sku_id)] = {
+            cart_dict[sku_id] = {
                 'count': count,
-                'selected': str(selected)
+                'selected': selected,
             }
 
             # 对字典进行加密
             carts_data = base64.b64encode(pickle.dumps(cart_dict)).decode()
             # 拼接
             response = http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'ok'})
-            response.set_cookie('carts', carts_data,max_age=3600*24*15)
+            response.set_cookie('carts', carts_data)
             # 返回
             return response
